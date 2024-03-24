1bee0200897d8a2156a1964f7abf3a7ac4c30159
Eduardo Nunes <eduardo.afonso.silva@ccc.ufcg.edu.br>
2024-03-24 15:05:05 -0300

    creating patterns for validation

---
+++ educa-facil/src/main/java/br/com/eduardonunesdev/educafacil/constants/RegexConstants.java
@@ -0,0 +1,8 @@
+package br.com.eduardonunesdev.educafacil.constants;
+
+public class RegexConstants {
+
+    public static final String VALID_USERNAME = "^[a-z]*$";
+
+    public static final String VALID_COURSE_CODE = "^[a-z-]*$";
+}
