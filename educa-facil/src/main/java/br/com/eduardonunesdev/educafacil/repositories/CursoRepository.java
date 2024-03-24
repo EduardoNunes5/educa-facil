63ded938a212d0b6b34e1fce795ceb556d74d7d1
Eduardo Nunes <eduardo.afonso.silva@ccc.ufcg.edu.br>
2024-03-24 20:07:13 -0300

    feat: create course

--- educa-facil/src/main/java/br/com/eduardonunesdev/educafacil/repositories/CursoRepository.java
+++ educa-facil/src/main/java/br/com/eduardonunesdev/educafacil/repositories/CursoRepository.java
@@ -1,2 +1,7 @@
-package br.com.eduardonunesdev.educafacil.repositories;public class CursoRepository {
+package br.com.eduardonunesdev.educafacil.repositories;
+
+import br.com.eduardonunesdev.educafacil.model.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CursoRepository extends JpaRepository<Course, String> {
 }
