dd2694fe0de0a87b77e57014d9a2713e26a7833d
Eduardo Nunes <eduardo.afonso.silva@ccc.ufcg.edu.br>
2024-04-01 15:28:39 -0300

    feat: adding login validation

--- educa-facil/src/main/java/br/com/eduardonunesdev/educafacil/services/AuthenticationService.java
+++ educa-facil/src/main/java/br/com/eduardonunesdev/educafacil/services/AuthenticationService.java
@@ -4,9 +4,12 @@ import br.com.eduardonunesdev.educafacil.dtos.authentication.LoginRequestDTO;
 import br.com.eduardonunesdev.educafacil.dtos.authentication.LoginResponseDTO;
 import br.com.eduardonunesdev.educafacil.model.User;
 import br.com.eduardonunesdev.educafacil.services.contracts.AuthenticatedUserService;
+import br.com.eduardonunesdev.educafacil.services.exceptions.BadRequestException;
+import br.com.eduardonunesdev.educafacil.services.exceptions.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
@@ -18,10 +21,15 @@ public class AuthenticationService implements AuthenticatedUserService {
     private final JwtService jwtService;
 
     public LoginResponseDTO authenticate(LoginRequestDTO loginRequestDTO){
-        var usernamePassword = new UsernamePasswordAuthenticationToken(loginRequestDTO.username(), loginRequestDTO.password());
-        var auth = authenticationManager.authenticate(usernamePassword);
-        var token = jwtService.generateToken((User) auth.getPrincipal());
-        return new LoginResponseDTO(token);
+        try {
+            var usernamePassword = new UsernamePasswordAuthenticationToken(loginRequestDTO.username(), loginRequestDTO.password());
+            var auth = authenticationManager.authenticate(usernamePassword);
+            var token = jwtService.generateToken((User) auth.getPrincipal());
+            return new LoginResponseDTO(token);
+        }
+        catch (AuthenticationException e){
+            throw new BadRequestException("Usuário não encontrado ou credenciais incorretas!");
+        }
     }
 
     @Override
